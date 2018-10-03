ebfd3fd45b96b4dfee7ba270e1f9d053f1d2d4ef
Dieter Hubau <dhubau@gmail.com>
2018-10-03 15:28:43 +0200

    back to flux we go!

--- src/main/java/be/ordina/function/MandelbrotCalculator.java
+++ src/main/java/be/ordina/function/MandelbrotCalculator.java
@@ -1,14 +1,15 @@
 package be.ordina.function;
 
+import reactor.core.publisher.Flux;
+
 import java.util.Arrays;
-import java.util.List;
 import java.util.function.Function;
 
 @SuppressWarnings("unused")
-public class MandelbrotCalculator implements Function<Grid, List<Calculation>> {
+public class MandelbrotCalculator implements Function<Flux<Grid>, Flux<Calculation>> {
 
 	@Override
-	public List<Calculation> apply(Grid grid) {
-		return Arrays.asList(new Calculation(100.0, 100.0, 255), new Calculation(0.0, 0.0, 255));
+	public Flux<Calculation> apply(Flux<Grid> grid) {
+		return Flux.fromIterable(Arrays.asList(new Calculation(100.0, 100.0, 255), new Calculation(0.0, 0.0, 255)));
 	}
 }
